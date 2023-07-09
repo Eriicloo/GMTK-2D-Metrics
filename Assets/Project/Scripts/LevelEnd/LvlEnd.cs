8e127e4b4a22256cdf927c28a4326cae6bbf8f72
David Soriano <david.soriano@enti.cat>
2023-07-10 00:00:21 +0200

    Fix Sounds / Collectibles / Flags / Locked levels / Animations

---
+++ Assets/Project/Scripts/LevelEnd/LvlEnd.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LvlEnd : MonoBehaviour
+{
+    [SerializeField] private Collectible _levelCollectible;
+    [SerializeField] private bool _isLastLevel;
+    
+    public Animator animator;
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            AudioManager.Instance.PlaySounds("Win");
+            animator.SetBool("IsActive", true);
+        }
+
+    }
+    private void ChangeLevel()
+    {
+        animator.SetBool("IsActive", false);
+
+        if (_levelCollectible._wasCollected)
+        {
+            PlayerPrefsManager.Instance.SaveGemForCurrentLevel();
+        }
+
+        PlayerPrefsManager.Instance.UpdateUnlockedLevels();
+
+        if (_isLastLevel)
+        {
+            SceneManager.LoadScene(0);
+        }
+        else
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+
+
+}
